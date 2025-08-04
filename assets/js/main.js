54b7b2b640f7ac134d8641e75173616e6441aefc
Vendula Martinková <vendaciki@seznam.cz>
2025-08-04 11:06:47 +0200

    osvětim,holandsko a opravy

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,58 +22,6 @@
 			}, 100);
 		});
 
-	// Menu.
-		// $menu
-		// 	.appendTo($body)
-		// 	.panel({
-		// 		delay: 500,
-		// 		hideOnClick: true,
-		// 		hideOnSwipe: true,
-		// 		resetScroll: true,
-		// 		resetForms: true,
-		// 		side: 'right',
-		// 		target: $body,
-		// 		visibleClass: 'is-menu-visible'
-		// 	});
-
-	// Search (header).
-		// var $search = $('#search'),
-		// 	$search_input = $search.find('input');
-
-		// $body
-		// 	.on('click', '[href="#search"]', function(event) {
-
-		// 		event.preventDefault();
-
-		// 		// Not visible?
-		// 			if (!$search.hasClass('visible')) {
-
-		// 				// Reset form.
-		// 					$search[0].reset();
-
-		// 				// Show.
-		// 					$search.addClass('visible');
-
-		// 				// Focus input.
-		// 					$search_input.focus();
-
-		// 			}
-
-		// 	});
-
-		// $search_input
-		// 	.on('keydown', function(event) {
-
-		// 		if (event.keyCode == 27)
-		// 			$search_input.blur();
-
-		// 	})
-		// 	.on('blur', function() {
-		// 		window.setTimeout(function() {
-		// 			$search.removeClass('visible');
-		// 		}, 100);
-		// 	});
-
 	// Intro.
 		var $intro = $('#intro');
 
